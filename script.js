3e5bf9be1c2d230fab6772b3a70e5ff4adcd7a98
linegommesen <linegommesen@MacBook-Pro-tilhrende-Line.local>
2021-08-31 12:01:37 +0200

    everything lowercase

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ async function getJson() {
   console.log(students);
 
   showStudents();
+  //   spellRight();
 }
 function showStudents() {
   let temp = document.querySelector("template");
@@ -15,11 +16,19 @@ function showStudents() {
   container.innerHTML = "";
   students.forEach((student) => {
     let clone = temp.cloneNode(true).content;
-    clone.querySelector(".name").innerHTML = student.fullname;
-    clone.querySelector(".gender").textContent = student.gender;
-    clone.querySelector(".house").textContent = student.house;
+    clone.querySelector(".name").innerHTML = student.fullname.toLowerCase();
+    clone.querySelector(".gender").textContent = student.gender.toLowerCase();
+    clone.querySelector(".house").textContent = student.house.toLowerCase();
+    // spellRight();
+    // function spellRight() {
+    //   const lowerCase = ".name".toLowerCase();
+    //   const upperCase = ".name"[0].toUpperCase() + lowerCase.substring(1);
+    //   console.log(upperCase);
+    //   ".name".innerHTML = `${upperCase}`;
+    // }
 
     container.appendChild(clone);
   });
 }
+
 getJson();
