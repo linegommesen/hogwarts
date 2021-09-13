4e32d0fae18e6f907424591718d8b4e82e5ba66a
linegommesen <linegommesen@MacBook-Pro-tilhrende-Line.local>
2021-09-13 14:35:18 +0200

    new script for list + list showed

--- newscript.js
+++ newscript.js
@@ -1,34 +1,66 @@
 "use strict";
 
-window.addEventListener("load", loadJson);
+window.addEventListener("load", start);
 
 let studentArray = [];
-const Student = {
-  firstName: "",
-  lastName: "",
-  middleName: "",
-  nickName: "",
-  house: "",
-};
-function loadJson() {
-  fetch(`https://petlatkea.dk/2021/hogwarts/students.json`)
-    .then((response) => response.json())
-    .then((data) => convertToArray(data));
-}
-function convertToArray(data) {
-  let list = document.querySelector(".list");
-  data.forEach((element) => {
-    const student = Object.create(Student);
-
-    const name = getNameParts(element.fullname);
-    console.log(name);
-    student.firstname = name.firstNameUppercase;
-    student.middlename = name.middleNameUppercase;
-    student.lastname = name.lastNameUppercase;
-    studentArray.push(student);
-  });
-}
 console.log("studentArray", studentArray);
+
+const Student = {
+  firstname: "",
+  lastname: "",
+  middlename: "",
+  nickname: "",
+  house: "",
+  gender: "",
+};
+function start() {
+  console.log("ready");
+  loadJson();
+}
+async function loadJson() {
+  const response = await fetch(`https://petlatkea.dk/2021/hogwarts/students.json`);
+  const jsonData = await response.json();
+  console.log(jsonData);
+
+  //   fetch(`https://petlatkea.dk/2021/hogwarts/students.json`)
+  //     .then((response) => response.json())
+  //     .then((jsonData) => prepareObjects(jsonData));
+
+  prepareObjects(jsonData);
+}
+function prepareObjects(jsonData) {
+  studentArray = jsonData.map(prepareObject);
+
+  displayList(studentArray);
+}
+function prepareObject(jsonObject) {
+  const student = Object.create(Student);
+
+  const name = getNameParts(jsonObject.fullname);
+  const cleanedHouse = getHouse(jsonObject.house);
+  student.firstname = name.firstNameUppercase;
+  student.middlename = name.middleNameUppercase;
+  student.lastname = name.lastNameUppercase;
+  student.house = cleanedHouse;
+  student.gender = jsonObject.gender;
+
+  //   console.log(student);
+  return student;
+}
+function displayList(students) {
+  document.querySelector(".list").innerHTML = "";
+
+  students.forEach(displayStudent);
+}
+function displayStudent(student) {
+  const clone = document.querySelector("template").content.cloneNode(true);
+
+  clone.querySelector(".name").textContent = student.firstname + " " + student.lastname;
+  clone.querySelector(".house").textContent = student.house;
+  clone.querySelector(".gender").textContent = student.gender;
+
+  document.querySelector(".list").appendChild(clone);
+}
 
 function getNameParts(fullname) {
   const trimmedName = fullname.trim();
@@ -46,9 +78,16 @@ function getNameParts(fullname) {
   } else if (middleNameUppercase === "") {
     middleNameUppercase = undefined;
   }
+
   return {
     firstNameUppercase,
     middleNameUppercase,
     lastNameUppercase,
   };
+}
+function getHouse(house) {
+  const trimmedHouse = house.trim();
+  // const lowerCase = trimmedHouse.toLowerCase();
+  const cleanedHouse = trimmedHouse[0].toUpperCase() + trimmedHouse.substring(1).toLowerCase();
+  return cleanedHouse;
 }
